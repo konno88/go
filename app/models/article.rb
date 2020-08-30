7e2d00b0a0f5bddb5195a3b59d6742009affb36e
今野郷 <xiangjinye1@gmail.com>
2020-08-31 04:37:28 +0900

    画像を投稿など機能を強化した

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,7 @@ class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
     has_one_attached :eyecatch
+    has_rich_text :content
 
     belongs_to :user
 
