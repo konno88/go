dc06e266f01255f3f417d2b6e4ad048177682cfc
今野郷 <xiangjinye1@gmail.com>
2020-08-30 19:09:45 +0900

    記事の一覧、投稿、編集、削除とログイン機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_075619) do
+ActiveRecord::Schema.define(version: 2020_08_30_081117) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
