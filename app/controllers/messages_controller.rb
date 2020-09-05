41dec5f24ac59c076d5391cd4ed15f155c333cab
今野郷 <xiangjinye1@gmail.com>
2020-09-06 01:41:59 +0900

    ダイレクトメッセージにて自分のコメントだけ消せるように

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,4 +9,11 @@ class MessagesController < ApplicationController
           redirect_back(fallback_location: root_path)
         end
     end
+
+    def destroy
+      @room = Room.find(params[:id])
+      message = current_user.messages.find(params[:id])
+      message.destroy!
+      redirect_to room_path(@room), notice: '削除しました'
+    end
 end
